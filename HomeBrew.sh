fe2c8fc93817fd73432c026f81ec5839dff8a584
nrigel <nick.rigel@gmail.com>
2020-01-07 14:20:47 -0500

    Update HomeBrew.sh

--- HomeBrew.sh
+++ HomeBrew.sh
@@ -8,11 +8,11 @@
 # 4. Navigate to local folder where you want to install
 # 5. Paste and run command in #2
 
-mkdir $PWD'/Applications' > /dev/null 2>&1
-cd $PWD'/Applications'
+mkdir ~/Applications > /dev/null 2>&1
+cd ~/Applications
 mkdir homebrew && curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C homebrew
 
-echo "alias brew='"$PWD"/Applications/homebrew/bin/brew'" >> ~/.bash_profile
+echo "alias brew='"~"/Applications/homebrew/bin/brew'" >> ~/.bash_profile
 source ~/.bash_profile
 
 echo 'finished'
